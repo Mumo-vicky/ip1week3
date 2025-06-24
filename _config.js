61e7b8a85d59d2e23d292baeb6771a49f6e32fd9
Mumo-vicky <mumovicky00@gmail.com>
2025-06-24 03:21:56 +0300

    _config.js file modification

--- _config.js
+++ _config.js
@@ -3,7 +3,7 @@ var config = {}
 // Update to have your correct username and password
 config.mongoURI = {
     production: 'mongodb+srv://vickymutua:5BqD6qZZuHOhp4o5@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://vickymutua:5BqD6qZZuHOhp4o5@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    development: 'mongodb+srv://vickymutua:5BqD6qZZuHOhp4o5@ipweek3cluster.kkfjqic.mongodb.net/?retryWrites=true&w=majority&appName=IPWeek3Cluster',
     test: 'mongodb+srv://vickmutua:5BqD6qZZuHOhp4o5@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
